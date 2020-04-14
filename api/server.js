450781cdabee6af9828886649dfb0b15e0edfcf8
Sam Ardis <ardis.productions@gmail.com>
2020-04-14 13:23:57 -0400

    Added router folder/file, coded server.js, working on queries

--- api/server.js
+++ api/server.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const Account = require('../router/accountRouter');
+
 const server = express();
 
+
 server.use(express.json());
+
+server.use('api/accounts', Account);
+
+server.get('/', (req, res) => {
+    res.status(200).json({api: ''})
+});
 
 module.exports = server;
